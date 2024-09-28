df54cd3806ac6b47124ff9b8e32275019058c520
mohamed_eid <eng.meid404@gmail.com>
2024-09-28 17:37:59 +0300

    V1: small fix on local-exec command

--- modules/06-instances-Bastion/main.tf
+++ modules/06-instances-Bastion/main.tf
@@ -11,9 +11,10 @@ resource "aws_key_pair" "bastion_key" {
   key_name   = "bastion_key"
   public_key = tls_private_key.bastion_key.public_key_openssh
 
-  provisioner "local-exec" {
-    command = "echo '${tls_private_key.bastion_key.private_key_pem}' > ${var.private_key_path} && chmod 600 ${var.private_key_path}"
-  }
+provisioner "local-exec" {
+  command = "echo '${tls_private_key.bastion_key.private_key_pem}' > ./bastion_key.pem && chmod 600 ./bastion_key.pem"
+}
+
 }
 
 # Create EC2 Instances in Public Subnets
